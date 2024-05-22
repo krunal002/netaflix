919c4fcdef0eeb966a5c79b5dff6214aeabf2e14
krunal002 <krunalmandlekar@gmail.com>
2024-05-22 08:12:40 +0530

    state variables used to store information of user

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Header } from "../components/Header";
 
 export const Login = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
     <div>
@@ -14,9 +17,7 @@ export const Login = () => {
           alt="cover"
         />
       </div>
-      <form
-        className="left-0 right-0 my-[30vh] mx-auto w-3/12 p-12 absolute bg-black opacity-90 flex flex-col items-center rounded"
-      >
+      <form className="left-0 right-0 my-[30vh] mx-auto w-3/12 p-12 absolute bg-black opacity-90 flex flex-col items-center rounded">
         <h1 className="text-3xl text-white mb-5 font-bold">
           {isLoggedIn ? "Login" : "Signup"}
         </h1>
@@ -25,17 +26,23 @@ export const Login = () => {
             <input
               type="text"
               placeholder="Enter name"
+              value={fullName}
+              onChange={(e) => setFullName(e.target.value)}
               className="p-3 my-1 rounded-sm outline-none bg-gray-800 text-white"
             />
           )}
           <input
             type="email"
             placeholder="E-mail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             className="p-3 my-1 rounded-sm outline-none bg-gray-800 text-white"
           />
           <input
             type="password"
             placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             className="p-3 my-1 rounded-sm outline-none bg-gray-800 text-white"
           />
           <button className="bg-red-700 text-white font-medium mt-6 p-3 rounded">
