d275ec15d55dbaa82373cc0ad589afa60f622e1c
krunal002 <krunalmandlekar@gmail.com>
2024-05-23 22:20:06 +0530

    browser page restricted

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import { useEffect } from "react";
+import { Header } from "../components/Header";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 export const Browse = () => {
+  const user = useSelector((store) => store.app.user);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/");
+    }
+  }, [navigate, user]);
+
   return (
-    <div>Browse</div>
-  )
-}
+    <div>
+      <Header />
+    </div>
+  );
+};
