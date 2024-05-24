9c6ca59428f6a1b0b5068b9bfb0c8d713c63af5c
krunal002 <krunalmandlekar@gmail.com>
2024-05-25 00:27:48 +0530

    made video title dynamic

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import { useSelector } from 'react-redux'
 import { VideoTitle } from './VideoTitle'
 import { VideoBackground } from './VideoBackground'
 
 export const MainContainer = () => {
+  const movies = useSelector(store => store.movie?.nowPlayingMovies)
+  if(!movies) return // early return in react
+  const movie = movies[1];
+
   return (
     <div>
-      <VideoTitle/>
+      <VideoTitle title={movie.original_title} overview={movie.overview}/>
       <VideoBackground/>
     </div>
   )
